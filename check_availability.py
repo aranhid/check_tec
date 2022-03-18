526f0ae5bc60af9b46e1f493447579c5183a3bc0
Владислав Цыбуля <aranhid@yandex.ru>
2022-03-18 18:29:16 +0800

    Change logic of checking gaps

--- check_availability.py
+++ check_availability.py
@@ -2,7 +2,6 @@ import pprint
 import argparse
 import pandas as pd
 import plotly.express as px
-from math import isclose
 from types import MappingProxyType
 from gnss_tec import rnx, BAND_PRIORITY
 from datetime import datetime, timedelta
@@ -29,41 +28,45 @@ def find_common_gaps(df: pd.DataFrame, interval: timedelta):
     all_available_times = sorted(all_available_times)
     all_available_times = pd.DataFrame(list(all_available_times), columns=('Timestamp',))
     all_available_times['Duration'] = all_available_times.diff()
-    all_sats_gaps = all_available_times[all_available_times['Duration'] > interval].copy()
+    common_gaps = all_available_times[all_available_times['Duration'] > interval].copy()
 
-    all_sats_gaps['Duration'] = all_sats_gaps['Duration'] - interval
-    all_sats_gaps['Timestamp'] = all_sats_gaps['Timestamp'] - all_sats_gaps['Duration']
+    common_gaps['Duration'] = common_gaps['Duration'] - interval
+    common_gaps['Timestamp'] = common_gaps['Timestamp'] - common_gaps['Duration']
 
-    all_sats_gaps = all_sats_gaps.reset_index(drop=True)
+    common_gaps = common_gaps.reset_index(drop=True)
 
-    return all_sats_gaps
+    return common_gaps
 
 
-def find_gaps_in_sats(df: pd.DataFrame, interval: timedelta, common_gaps: pd.DataFrame):
+def prepare_dataframe(df: pd.DataFrame, common_gaps_df: pd.DataFrame, interval: timedelta):
+    all_available_times = df['Timestamp'].unique()
+    all_available_times.sort()
+    frequency = str(interval.seconds) + 'S'
+    prototype_df = pd.DataFrame(pd.date_range(start=all_available_times[0], end=all_available_times[-1], freq=frequency), columns=('Timestamp',))
+    prototype_df['Status'] = 'None'
+
+    for index in common_gaps_df.index:
+        gap_start = common_gaps_df.loc[index]['Timestamp']
+        gap_end = common_gaps_df.loc[index]['Timestamp'] + common_gaps_df.loc[index]['Duration']
+        gaps_df = prototype_df[prototype_df['Timestamp'] >= gap_start]
+        gaps_df = gaps_df[gaps_df['Timestamp'] < gap_end]
+        prototype_df.loc[gaps_df.index, 'Status'] = 'Common gap'
+
+
+    ret_df = pd.DataFrame()
+
     sats = df['Satellite'].unique()
-
-    gaps_by_sat = {}
-
+    sats.sort()
     for sat in sats:
         sat_df = df[df['Satellite'] == sat]
-        sat_df = sat_df.sort_values(by=['Timestamp'])
-        sat_df['Duration'] = sat_df['Timestamp'].diff()
-        gaps = sat_df[sat_df['Duration'] > interval]
-        gaps = gaps.drop(columns=['Satellite',])
-        gaps['Duration'] = gaps['Duration'] - interval
-        gaps['Timestamp'] = gaps['Timestamp'] - gaps['Duration']
-        gaps = gaps.reset_index(drop=True)
+        prototype_df_copy = prototype_df.copy()
+        prototype_df_copy['Satellite'] = sat
+        prototype_df_copy['isin'] = prototype_df_copy['Timestamp'].isin(sat_df['Timestamp'])
+        prototype_df_copy.loc[prototype_df_copy[prototype_df_copy['isin'] == True].index, 'Status'] = 'Data'
+        prototype_df_copy = prototype_df_copy.drop(columns=['isin',])
+        ret_df = pd.concat([ret_df, prototype_df_copy], ignore_index=True)
 
-
-        merge_df = gaps.merge(common_gaps, on=['Timestamp','Duration'], 
-                   how='left', indicator=True)
-
-        gaps_without_common_gaps = merge_df[merge_df['_merge'] == 'left_only']
-        gaps_without_common_gaps = gaps_without_common_gaps.drop(columns=['_merge',])
-        gaps_without_common_gaps = gaps_without_common_gaps.reset_index(drop=True)
-        gaps_by_sat[sat] = gaps_without_common_gaps
-
-    return gaps_by_sat
+    return ret_df
 
 
 def check_density_of_gaps(df: pd.DataFrame, window_size: str, max_gap_num: int):
@@ -72,26 +75,45 @@ def check_density_of_gaps(df: pd.DataFrame, window_size: str, max_gap_num: int):
     windows = []
     i = 0
 
-    if len(work_df):
-        for window in work_df.rolling(window_size):
-            if len(window) > max_gap_num:
-                if not len(windows) > 0:
-                    windows.append(set())
-                if len(windows[i]) == 0 or (window.index[0], window.loc[window.index[0]]['Duration']) in windows[i]:
-                    for index in window.index:
-                        windows[i].add((index, window.loc[index]['Duration']))
-                else:
-                    windows.append(set())
-                    i += 1
-                    for index in window.index:
-                        windows[i].add((index, window.loc[index]['Duration']))
+    start_time = None
+
+    for window in work_df.rolling(window_size):
+        if len(window) == (len(window[window['Status'] == 'None']) + len(window[window['Status'] == 'Common gap'])):
+            continue
+
+        if len(window) > 1:
+            check_left = window.iloc[0 : len(window) // 2]
+            check_right = window.iloc[len(window) // 2 : len(window)]
+
+            if len(check_left[check_left['Status'] == 'Data']) and len(check_right[check_right['Status'] == 'None']) >= len(window) // 2:
+                if len(windows) > i:
+                    windows[i].difference_update(window.index.to_pydatetime())
+                continue
+
+            if window.iloc[-1]['Status'] == 'Data' and len(window[window['Status'] == 'Data']) == 1:
+                start_time = window.iloc[-1].name
+        
+        gaps = window[window['Status'] == 'None']
+        if len(gaps) > max_gap_num:
+            if not len(windows) > 0:
+                windows.append(set())
+            if len(windows[i]) == 0 or gaps.index[0] in windows[i]:
+                windows[i].update(gaps.index.to_pydatetime())
+                if start_time:
+                    for val in list(windows[i]):
+                        if val < start_time:
+                            windows[i].discard(val)
+            else:
+                windows.append(set())
+                i += 1
+                windows[i].update(gaps.index.to_pydatetime())
 
     windows = [sorted(window) for window in windows]
     
     ret = []
     for window in windows:
         if len(window):
-            ret.append((window[0][0], window[-1][0] + window[-1][1]))
+            ret.append((window[0], window[-1]))
 
     return ret
 
@@ -104,38 +126,19 @@ def create_simple_plot(df: pd.DataFrame, interval: timedelta):
     fig.show()
 
 
-def create_debug_plot(df: pd.DataFrame, interval: timedelta, common_gaps: pd.DataFrame, gaps_by_sat: dict, problems_by_sat: dict, filename: str = None, show: bool = False):
+def create_debug_plot(df: pd.DataFrame, problems_by_sat: dict, interval: timedelta, filename: str = None, show: bool = False):
     work_df = df.copy()
-    work_df = work_df.sort_values(by=('Satellite'))
-    work_df['Status'] = 'Normal'
     work_df['Timestamp end'] = work_df['Timestamp'] + interval
 
-    for sat in work_df['Satellite'].unique():
-        common_gaps_copy = common_gaps.copy()
-        common_gaps_copy['Status'] = 'Common gap'
-        common_gaps_copy['Satellite'] = sat
-        common_gaps_copy['Timestamp end'] = common_gaps_copy['Timestamp'] + common_gaps_copy['Duration']
-        work_df = pd.concat([work_df, common_gaps_copy], ignore_index=True)
+    for sat in problems_by_sat.keys():
+        for problem in problems_by_sat[sat]:
+            problem_df = work_df[work_df['Satellite'] == sat]
+            problem_df = problem_df[problem_df['Timestamp'] >= problem[0]]
+            problem_df = problem_df[problem_df['Timestamp'] <= problem[1]]
+            work_df.loc[problem_df.index, 'Status'] = 'Problem'
 
-    for sat in gaps_by_sat_df.keys():
-        sat_gaps_copy = gaps_by_sat[sat]
-        sat_gaps_copy['Status'] = 'Satellite gap'
-        sat_gaps_copy['Satellite'] = sat
-        sat_gaps_copy['Timestamp end'] = sat_gaps_copy['Timestamp'] + sat_gaps_copy['Duration']
-        work_df = pd.concat([work_df, sat_gaps_copy], ignore_index=True)
-
-    for sat in problems_by_sat:
-        if len(problems_by_sat[sat]) > 0:
-            for problem in problems_by_sat[sat]:
-                sat_df = work_df[work_df['Satellite'] == sat]
-                sat_df = sat_df[sat_df['Timestamp'] >= problem[0]]
-                sat_df = sat_df[sat_df['Timestamp'] < problem[1]]
-                for index in sat_df.index:
-                    work_df.loc[index, ('Status',)] = 'Problem window'
-
-    discrete_map_resource = { 'Problem window': '#FF0000', 'Common gap': '#00FF00', 'Normal': '#0000FF', 'Satellite gap': '#FFFF00'}
-    fig = px.timeline(work_df, x_start='Timestamp',
-                      x_end='Timestamp end', y='Satellite', color='Status', color_discrete_map=discrete_map_resource)
+    discrete_map_resource = {'Problem': '#FF0000', 'Data': '#00b300', 'Common gap': '#0000FF', 'None': '#d9d507'}
+    fig = px.timeline(work_df, x_start='Timestamp', x_end='Timestamp end', y='Satellite', color='Status', color_discrete_map=discrete_map_resource)
     if show:
         fig.show()
     if filename:
@@ -163,17 +166,17 @@ if __name__ == '__main__':
         df = pd.concat([df, temp_df], ignore_index=True)
 
     common_gaps_df = find_common_gaps(df, interval)
-    common_problems = check_density_of_gaps(common_gaps_df, window_size, args.max_gap_num)
 
-    gaps_by_sat_df = find_gaps_in_sats(df, interval, common_gaps_df)
+    working_df = prepare_dataframe(df, common_gaps_df, interval)
+
     problems_by_sat = {}
-    for sat in gaps_by_sat_df.keys():
-        problems_by_sat[sat] = check_density_of_gaps(gaps_by_sat_df[sat], window_size, args.max_gap_num)
-    
-    if args.plot_show or not args.plot_file == None:
-        create_debug_plot(df=df, interval=interval, common_gaps=common_gaps_df, gaps_by_sat=gaps_by_sat_df, problems_by_sat=problems_by_sat, show=args.plot_show, filename=args.plot_file)
+    for sat in working_df['Satellite'].unique():
+        problems_by_sat[sat] = check_density_of_gaps(working_df[working_df['Satellite'] == sat], window_size, args.max_gap_num)
 
-    print('Common problems')
-    pprint.pprint(common_problems)
-    print('Problems by satellite')
-    pprint.pprint(problems_by_sat)
+    if args.plot_show or not args.plot_file == None:
+        create_debug_plot(working_df, problems_by_sat, interval, show=args.plot_show, filename=args.plot_file)
+
+    # print('Common problems')
+    # pprint.pprint(common_problems)
+    # print('Problems by satellite')
+    # pprint.pprint(problems_by_sat)
