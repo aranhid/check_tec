eab40b80f6d05c89f9f20b48c108d13344a887c6
Владислав Цыбуля <aranhid@yandex.ru>
2022-05-19 03:35:27 +0800

    Another one realtime check tec

---
+++ processStream.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+
+"""Consumes stream for printing all messages to the console.
+"""
+
+import argparse
+from datetime import datetime, timedelta
+import json
+import base64
+import queue
+import sys
+import socket
+import threading
+import pandas as pd
+import matplotlib.pyplot as plt
+from pyrtcm import RTCMMessage
+from confluent_kafka import Consumer, KafkaError, KafkaException
+from gnss_tec import tec, gnss
+from pprint import pprint
+
+from check_tec import check_phase_tec, check_range_tec, plot_check_phase_tec, plot_check_range_tec
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.WARNING)
+
+last_timestamp = None
+window_size = 3600
+satellites_data = {}
+q = queue.Queue()
+create_figure = queue.Queue()
+plt.ion()
+
+
+def gpsmsectotime(msec,leapseconds) -> datetime:
+    datetimeformat = "%Y-%m-%d %H:%M:%S"
+    epoch = datetime.strptime("1980-01-06 00:00:00",datetimeformat)
+    tdiff = datetime.utcnow() - epoch  + timedelta(seconds=(leapseconds - 19))
+    gpsweek = tdiff.days // 7 
+    t = epoch + timedelta(weeks=gpsweek) + timedelta(milliseconds=msec) - timedelta(seconds=(leapseconds - 19))
+    return t
+
+
+def msg_process(msg, topic):
+    if not msg is None:
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                # End of partition event
+                sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
+                                (msg.topic(), msg.partition(), msg.offset()))
+            elif msg.error().code() == KafkaError.UNKNOWN_TOPIC_OR_PART:
+                sys.stderr.write('Topic unknown, creating %s topic\n' %
+                                (topic))
+            elif msg.error():
+                raise KafkaException(msg.error())
+        else:
+            q.put(msg)
+
+
+def update_sat_data(sat, timestamp, phase_tec, p_range_tec):
+    df_data = [timestamp, sat, phase_tec, p_range_tec]
+
+    local_df = pd.DataFrame(data=[df_data], columns=('Timestamp', 'Satellite', 'Phase tec', 'P range tec'))
+
+    if sat not in satellites_data.keys():
+        satellites_data[sat] = {
+            'data': local_df,
+            'figure_phase': None,
+            'figure_range': None,
+            'checked_phase': None,
+            'checked_range': None,
+        }
+        return
+
+    satellites_data[sat]['data'] = pd.concat([satellites_data[sat]['data'], local_df], ignore_index=True)
+
+
+def check_phase(sat, plot = False):
+    df = satellites_data[sat]['data']
+    phase_tec_problems = []
+
+    checked_phase = check_phase_tec(df, std_mult=3.5)
+    satellites_data[sat]['checked_phase'] = checked_phase
+    if not checked_phase.empty:
+        red_phase_tec = checked_phase[checked_phase['Color'] == 'red']
+        phase_tec_problems = list(zip(red_phase_tec['Timestamp'].values, red_phase_tec['Phase tec'].values))
+
+    pprint(phase_tec_problems)
+
+def check_range(sat, plot = False):
+    df = satellites_data[sat]['data']
+    range_tec_problems = []
+
+    checked_range = check_range_tec(df, std_mult=3.5)
+    satellites_data[sat]['checked_range'] = checked_range
+    if not checked_range.empty:
+        red_range_tec = checked_range[checked_range['Color'] == 'red']
+        range_tec_problems = list(zip(red_range_tec['Timestamp'].values, red_range_tec['P range tec'].values))
+
+    pprint(range_tec_problems)
+
+
+def worker():
+    global last_timestamp
+
+    while True:
+        msg = q.get()
+        val = msg.value()
+        rtcm_msg = RTCMMessage(payload=val)
+
+        if rtcm_msg.identity == '1004':
+            delimiter = 299792.46
+            speed_of_light = 299792458
+
+            sat_id = rtcm_msg.DF009_01
+            sat = f'G{sat_id:02}'
+
+            p_range_1 = rtcm_msg.DF011_01 + delimiter * rtcm_msg.DF014_01
+            p_range_2 = p_range_1 + rtcm_msg.DF017_01
+
+            f1 = gnss.FREQUENCY.get('G').get(1)
+            f2 = gnss.FREQUENCY.get('G').get(2)
+
+            phase_range_1 = p_range_1 + rtcm_msg.DF012_01
+            phase_range_2 = p_range_1 + rtcm_msg.DF018_01
+
+            phase_1 = phase_range_1 / (speed_of_light / f1)
+            phase_2 = phase_range_2 / (speed_of_light / f2)
+            
+            t = tec.Tec(datetime.now(), 'GPS', sat)
+            t.p_range = {1: p_range_1, 2: p_range_2}
+            t.p_range_code = {1: 'C1C', 2: 'C2W'}
+            p_range_tec = t.p_range_tec
+
+            t.phase = {1: phase_1, 2: phase_2}
+            t.phase_code = {1: 'L1C', 2: 'L2S'}
+            phase_tec = t.phase_tec
+
+            timestamp = gpsmsectotime(rtcm_msg.DF004, 37)
+            last_timestamp = timestamp
+
+            print(sat)
+            update_sat_data(sat, timestamp, phase_tec, p_range_tec)
+            check_phase(sat)
+            check_range(sat)
+            
+        q.task_done()
+
+
+def get_message():
+    global consumer
+    global topic
+    try:
+        while True:
+            messages = consumer.consume(10, 1)
+            for msg in messages:
+                msg_process(msg, topic)
+    except RuntimeError:
+        pass
+
+
+def main():
+    global consumer
+    global topic
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('host', type=str, help='Host of the Kafka broker')
+    parser.add_argument('topic', type=str, help='Name of the Kafka topic to stream.')
+
+    args = parser.parse_args()
+
+    host = args.host
+    topic = args.topic
+
+    conf = {'bootstrap.servers': f'{host}:9092',
+            'default.topic.config': {'auto.offset.reset': 'smallest'},
+            'group.id': socket.gethostname()}
+
+    wrkr = threading.Thread(target=worker, daemon=True)
+    wrkr.start()
+
+    consumer = Consumer(conf)
+    consumer.subscribe([topic])
+    msg_t= threading.Thread(target=get_message, daemon=True)
+    msg_t.start()
+
+    running = True
+
+    try:
+        while running:
+            for sat in list(satellites_data.keys()):
+                time_border = last_timestamp - timedelta(seconds=window_size)
+                satellites_data[sat]['data'] = satellites_data[sat]['data'][satellites_data[sat]['data']['Timestamp'] >= time_border]
+                if satellites_data[sat]['data'].empty:
+                    if not satellites_data[sat]['figure_phase'] is None:
+                        plt.close(satellites_data[sat]['figure_phase'])
+                    
+                    if satellites_data[sat]['figure_range'] is None:
+                        plt.close(satellites_data[sat]['figure_range'])
+                    satellites_data.pop(sat)
+                    continue
+
+                if satellites_data[sat]['figure_phase'] is None:
+                    satellites_data[sat]['figure_phase'] = plt.figure()
+
+                if satellites_data[sat]['figure_range'] is None:
+                    satellites_data[sat]['figure_range'] = plt.figure()
+
+                df = satellites_data[sat]['data']
+                fig_phase = satellites_data[sat]['figure_phase']
+                checked_phase = satellites_data[sat]['checked_phase']
+                fig_range = satellites_data[sat]['figure_range']
+                checked_range = satellites_data[sat]['checked_range']
+
+                # if not df.empty and not checked_phase is None and not checked_range is None:
+                plot_check_phase_tec(df, checked_phase, fig_phase, sat=sat)
+                plot_check_range_tec(df, checked_range, fig_range, sat=sat)
+            plt.pause(1)
+
+
+    except KeyboardInterrupt:
+        pass
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        # Close down consumer to commit final offsets.
+        consumer.close()
+        q.join()
+
+
+if __name__ == "__main__":
+    main()
