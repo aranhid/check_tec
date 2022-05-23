48016571a3753101c0c6e2686170ff5a51c303a3
Владислав Цыбуля <aranhid@yandex.ru>
2022-05-23 15:19:24 +0800

    realtime_plot_tec: update

--- realtime_plot_tec.py
+++ realtime_plot_tec.py
@@ -1,19 +1,19 @@
+import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.errors import EmptyDataError
 from matplotlib.animation import FuncAnimation
-
 from check_tec import plot_check_phase_tec, plot_check_range_tec
 
-# function to update the data
+
 def update_phase(i):
     try:
         satellites_dataframe = pd.read_csv('satellites_dataframe.csv')
-        phase_problems = pd.read_csv('problems_phase.csv')
-        range_problems = pd.read_csv('problems_range.csv')
-    except:
-        print('error reading file')
+        if mode == 'phase':
+            phase_problems = pd.read_csv('problems_phase.csv')
+        else:
+            range_problems = pd.read_csv('problems_range.csv')
 
-    try:
         sat_df = satellites_dataframe[satellites_dataframe['Satellite'] == sat]
         if not sat_df.empty:
             if mode == 'phase':
@@ -25,17 +25,31 @@ def update_phase(i):
                     sat_range_problems_df = range_problems[range_problems['Satellite'] == sat]
                     plot_check_range_tec(sat_df, sat_range_problems_df, fig, sat=sat)
         else:
+            print(f'There is no data for {sat}, exit.')
             exit()
+    except EmptyDataError as e:
+        print('error reading file')
     except Exception as e:
         print(e)
 
 
-fig = plt.figure()
-sat = 'G05'
-mode = 'phase'
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('satellite', type=str, help='The name of the satellite, for example G05')
+    parser.add_argument('mode', type=str, default='phase', help='"phase" or "range"')
 
-# animate
-ani = FuncAnimation(fig, update_phase, interval=100)
+    args = parser.parse_args()
 
-# fig.tight_layout()
-plt.show()
+    global fig, sat, mode
+    sat = args.satellite
+    mode = args.mode
+
+    fig = plt.figure()
+    ani = FuncAnimation(fig, update_phase, interval=100)
+
+    fig.set_tight_layout(True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
